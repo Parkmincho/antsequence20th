823d0d4794f6d3709d4d03eb9e0bde1f29db0996
Mincho Park <s2018s28@e-mirim.hs.kr>
2020-05-12 18:35:20 +0900

    수열 세기

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,48 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
+            int select = 0;
+            int count = 1;
+            int a = 0;
+            int b = 0;
+            Console.Write("몇 번째 수열? : ");
+            select = int.Parse(Console.ReadLine());
+
+            int[] array1 = new int[500];
+            int[] array2 = new int[500];
+
+            array1[0] = 1;
+
+
+            for (int i = 0; i < select; i++)
+            {
+                while (array1[a] != 0)
+                {
+                    if (array1[a] == array1[a + 1])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        array2[b] = array1[a];
+                        array2[b + 1] = count;
+                        count = 1;
+                        b = b + 2;
+                    }
+                    a++;
+                }
+                
+                Array.Copy(array2, array1, array2.Length);
+                a = 0;
+                b = 0;
+                Console.Write(i + 1 + "번째=");
+                foreach (var arr in array1)
+                {
+                    if (arr == 0) break;
+                    Console.Write(arr);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
